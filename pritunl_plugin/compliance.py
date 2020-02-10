aeb66e9f8b8c9e38f512308bc0b60e08a82f8dec
Gerwout van der Veen <gerwoutvdveen@gmail.com>
2020-02-10 16:52:59 +0100

    Send more data to compliance checker -in preparation of building an audit log-

--- pritunl_plugin/compliance.py
+++ pritunl_plugin/compliance.py
@@ -2,6 +2,11 @@ import requests
 from itsdangerous import TimedJSONWebSignatureSerializer, BadSignature, SignatureExpired
 import configparser, os
 from pritunl import logger
+from pritunl.host.host import Host
+from pritunl.server.server import Server
+from pritunl.organization.organization import Organization
+
+import json
 
 def get_configuration():
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -40,11 +45,32 @@ def verify_api_auth_token(token):
 # connection and None if allowed or a string with reason if not allowed.
 def user_connect(host_id, server_id, org_id, user_id, host_name,
         server_name, org_name, user_name, remote_ip, mac_addr, platform, device_id, device_name, password, **kwargs):
-    host_name = host_name.upper()
+    host = Host(id=host_id).dict()
+    server = Server(id=server_id).dict()
+    org = Organization(id=org_id)
+    user = org.get_user(user_id).dict()
+    org = org.dict()
+    server_has_ip6 = server.get('ipv6', False)
+    server_protocol = str(server.get('protocol', ""))
+    server_port = str(server.get('port', ""))
+    server_name = str(server.get('name', ""))
+    server_status = str(server.get('status', ""))
+    host_ip6 = str(host.get('local_addr6', ""))
+    host_ip4 = str(host.get('local_addr', ""))
+    host_connect_ip = str(host.get('public_addr', ""))
+    host_name = str(host.get('name', "")).upper()
+    user_name = str(user.get('name', ""))
+    user_has_pin = str(user.get('pin', False))
+    user_is_disabled = str(user.get('disabled', False))
+    user_email = str(user.get('email', ""))
+    org_name = str(org.get('name', ""))
 
     json_dict = {"host_id": host_id, "server_id": str(server_id), "org_id": str(org_id), "user_id": str(user_id), "host_name": host_name,
                  "server_name": server_name, "org_name": org_name, "user_name": user_name, "remote_ip": remote_ip,
-                 "mac_addr": mac_addr, "platform": platform, "device_id": device_id, "device_name": device_name}
+                 "mac_addr": mac_addr, "platform": platform, "device_id": device_id, "device_name": device_name, "server_has_ip6": server_has_ip6,
+                 "server_protocol": server_protocol, "server_port": server_port, "server_status": server_status, "host_ip6": host_ip6,
+                 "host_ip4": host_ip4, "host_connect_ip": host_connect_ip, "user_has_pin": user_has_pin, "user_disabled": user_is_disabled,
+                 "user_email": user_email}
 
     token = generate_auth_token(json_dict)
     auth_token_dict = {"auth_token": token}
